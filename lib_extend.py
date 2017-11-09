aecf812ca39673db3c2dbd30970e440e8fc947a7
Hanjara Cahya Adhyatma <mangatkk@gmail.com>
2017-11-09 18:22:49 +0700

    Add File Extend

--- lib_extend.py
+++ lib_extend.py
@@ -2,3 +2,7 @@ def extended(i):
     if i == 0:
         i = 0
     return i
+
+
+def otherex():
+    print("their")
--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+from lib_extend import *
+
+# main program
 if __name__ == "__main__":
+    extended(2)
     print("Hello")
 
